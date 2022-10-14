af98cb2000b953e58bd563e29ae000b17d766b57
Nasser <nasser.kessas@outlook.com>
2022-10-14 16:50:31 +1000

    Removed unneeded data and added README

---
+++ README.md
@@ -0,0 +1,18 @@
+# Flag quiz
+
+## Overview
+
+A web-based flag to countries game made with JS
+[Try a demo](https://flag-quiz-nk.netlify.app/)
+![Flag-quiz](./docs/flag%20quiz.gif)
+
+## Todo
+- Add difficulty levels (time, flag difficulty)
+
+## Acknoledgements
+Created by Nasser Kessas
+
+## Contributing
+PRs accepted, feel free to dive in! [Open an issue](https://github.com/nasserkessas/js-flags-quiz/issues/new) or submit PRs.
+
+Js Flags Quiz follows the [Contributor Covenant](http://contributor-covenant.org/version/1/3/0/) Code of Conduct.
--- main.js
+++ main.js
@@ -1,77 +1,74 @@
 let correct;
 let amtCorrect = 0;
-let seconds = 10;
+let seconds = 20;
 let amtIncorrect = 0;
 function main() {
-    let options = [];
-    const maxOptions = 4;
+  let options = [];
+  const maxOptions = 4;
 
-    while (options.length < maxOptions) {
-        let country = getRandomCountry()
-        if (options.indexOf(country) === -1) {
-            options.push(country)
-        }
+  while (options.length < maxOptions) {
+    let country = getRandomCountry();
+    if (options.indexOf(country) === -1) {
+      options.push(country);
     }
-    correct = options[Math.round(Math.random() * (options.length - 1))];
-    for (let i = 0; i < options.length; i++) {
-        get(`option${i + 1}label`).innerHTML = options[i].name;
-        get(`option${i + 1}input`).value = options[i].name
-        get(`option${i + 1}input`).checked = false;
-    }
+  }
+  correct = options[Math.round(Math.random() * (options.length - 1))];
+  for (let i = 0; i < options.length; i++) {
+    get(`option${i + 1}label`).innerHTML = options[i].name;
+    get(`option${i + 1}input`).value = options[i].name;
+    get(`option${i + 1}input`).checked = false;
+  }
 
-    get("flag").src = correct.flag;
+  get("flag").src = correct.flag;
 }
 
 function getRandomCountry() {
-    return countries[Math.round(Math.random() * (countries.length - 1))];
+  return countries[Math.round(Math.random() * (countries.length - 1))];
 }
 
 function get(id) {
-    return document.getElementById(id);
+  return document.getElementById(id);
 }
 
 function check() {
-    let input;
-    try {
-        input = document.querySelector('input[name = "option"]:checked').value;
-    } catch{ return }
-    if (input === correct.name) {
-        console.log("correct");
-        amtCorrect++;
-        get("score").innerHTML = amtCorrect;
-    }
-    else {
-        console.log(`incorrect, answer was ${correct.name}`);
-        amtIncorrect++;
-    }
-    main();
+  let input;
+  try {
+    input = document.querySelector('input[name = "option"]:checked').value;
+  } catch {
+    return;
+  }
+  if (input === correct.name) {
+    amtCorrect++;
+    get("score").innerHTML = amtCorrect;
+  } else {
+    amtIncorrect++;
+  }
+  main();
 }
 
 function finish() {
-    clearInterval(checkInterval);
-    get("alert").style.display = "block";
-    get("card").style.display = "none";
-    get("alertscore").innerHTML = amtCorrect;
-    let percentage = Math.round(amtCorrect / (amtCorrect + amtIncorrect) * 100);
-    if (isNaN(percentage)) percentage = 100;
-    get("alertaccuracy").innerHTML = `${percentage}%`;
-    console.log(amtCorrect);
-    console.log("time up");
+  clearInterval(checkInterval);
+  get("alert").style.display = "block";
+  get("card").style.display = "none";
+  get("alertscore").innerHTML = amtCorrect;
+  let percentage = Math.round((amtCorrect / (amtCorrect + amtIncorrect)) * 100);
+  if (isNaN(percentage)) percentage = 100;
+  get("alertaccuracy").innerHTML = `${percentage}%`;
 }
 
 function timer() {
-    setTimeout(finish, seconds * 1000);
-    get("time").innerHTML = seconds;
-    let countdown = setInterval(function () {
-        seconds--;
-        get("time").textContent = seconds;
-        if (seconds <= 0) clearInterval(countdown);
-        if (seconds === 5) get("time").style.color = "#ff0000"
-    }, 1000);
+  setTimeout(finish, seconds * 1000);
+  get("time").innerHTML = seconds;
+  let countdown = setInterval(function () {
+    seconds--;
+    get("time").textContent = seconds;
+    if (seconds <= 0) clearInterval(countdown);
+    if (seconds === 5) get("time").style.color = "#ff0000";
+  }, 1000);
 }
 
 function refresh() {
-    location = location;
+  location = location;
 }
 
 let checkInterval = setInterval(check, 50);
